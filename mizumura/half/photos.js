bbe1b6c37acee3eeb9e7efd62b9c39fdbd79ed36
yamate-casino <114626397+yamate-casino@users.noreply.github.com>
2024-12-17 22:55:32 +0900

    photos.js を更新

--- mizumura/half/photos.js
+++ mizumura/half/photos.js
@@ -18,14 +18,14 @@ if(data == 'now'){
     e_d = data;
     j_d = "学校";
     p_d = ['IMG_9182.JPG','IMG_9205.JPG','IMG_1792.JPG','IMG_9937.JPG','IMG_9187.JPG','IMG_1789.JPG','IMG_1790.JPG','IMG_1791.JPG','IMG_1787.JPG','IMG_0820.JPG','IMG_0047.JPG','IMG_1782.JPG'];
-    id=['山手学院','山手学院','山手学院','山手学院','山手学院','山手学院','山手学院','電車','山手学院'];
-    id2=['YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','Train','YamateGakuin']
+    id=['山手学院','山手学院','山手学院','山手学院','山手学院','山手学院','山手学院','山手学院','山手学院','山手学院','電車','山手学院'];
+    id2=['YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','Train','YamateGakuin'];
 }else if(data == "date"){
     e_d = data;
     j_d = "二人の時間";
     p_d = ['IMG_1742.JPG','IMG_1743.JPG','IMG_1744.JPG','IMG_1745.JPG','IMG_1746.JPG','IMG_1784.JPG','IMG_1785.JPG','IMG_8726.JPG'];
     id=['山手学院','山手学院','山手学院','山手学院','山手学院','大船駅','大船駅','ガスト'];
-    id2=['YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','Ofuna ','Ofuna','Gust']
+    id2=['YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','YamateGakuin','Ofuna ','Ofuna','Gusto']
 }
 //document.getElementById("sp").innerHTML = 'e_d';
 document.getElementById("pre").innerHTML = j_d;
